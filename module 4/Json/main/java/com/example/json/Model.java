992e0b9942429751180d037cbd13e0fc47a90f3c
heettops <109722278+heettops@users.noreply.github.com>
2022-09-16 11:27:44 +0530

    Add files via upload

---
+++ module 4/Json/main/java/com/example/json/Model.java
@@ -0,0 +1,38 @@
+package com.example.json;
+
+public class Model
+{
+    String name,Realname,team,image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealname() {
+        return Realname;
+    }
+
+    public void setRealname(String realname) {
+        Realname = realname;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
