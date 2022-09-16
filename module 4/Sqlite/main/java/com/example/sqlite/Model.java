d4d91438d9d73037d2af635f79982662d677cd12
heettops <109722278+heettops@users.noreply.github.com>
2022-09-16 11:24:55 +0530

    Add files via upload

---
+++ module 4/Sqlite/main/java/com/example/sqlite/Model.java
@@ -0,0 +1,32 @@
+package com.example.sqlite;
+
+public class Model {
+
+    int id;
+    String name,phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
+
